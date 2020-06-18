991be5bb41302b32d3cfb308efc07b744caf413b
Don Townsend <don@teamairship.com>
2020-06-18 18:10:29 -0400

    further tweak styling / layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,13 @@ const Header = () => {
     <div className="header-top">
       <img src={logo} className="App-logo" alt="logo" />
       <h1 class="App-name">
-        R E A C T&nbsp;&nbsp;H E L P E R S
+        R E A C T
+        <br/>
+        <small>
+          <small>
+            H E L P E R S
+          </small>
+        </small>
       </h1>
     </div>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,11 +3,17 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <p>
-      React Helpers - a sandbox for trying out new utils,
-      new component patterns, new state management, and
-      anything else one can possibly think of.
-    </p>
+    <>
+      <h2>React Helpers - Home</h2>
+      <p>
+        This is a sandbox for trying out new utils,
+        new component patterns, new state management, and
+        anything else one can possibly think of.
+      </p>
+      <p>
+        Use the left navigation to find your way.
+      </p>
+    </>
   );
 };
 
